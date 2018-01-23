08e47278547c69c15ed4d5f995508669b6f0868d
Luis Garcia <luigi311.lg@gmail.com>
2018-01-23 12:16:11 +0000

    Add optional flag to only display btc prices or usd prices

--- coin_prices.py
+++ coin_prices.py
@@ -1,5 +1,11 @@
+import argparse
 import urllib.request, json, time, os
 from time import gmtime, strftime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--btc", help="Display Bitcoin prices", action="store_true")
+parser.add_argument("--usd", help="Display USD prices", action="store_true")
+args = parser.parse_args()
 
 def check_prices(names):
     with urllib.request.urlopen("https://bittrex.com/api/v1.1/public/getmarketsummaries") as url:
@@ -24,14 +30,24 @@ try:
         names = ["btc","strat","xvg","snt","gnt","pay","steem","neo"]
         data = check_prices(names)
         os.system('clear')
+
         print (strftime("%m-%d-%y %H:%M:%S", gmtime()))
         for i in names:
-             if i == 'btc':
-                 print (i,":\t",data[i],"USD")
+             usd_prices = ["{:.4f}".format(float(data[i])*float(data['btc'])),'USD']
+             btc_prices = [data[i],"BTC"]
+
+             if args.usd and args.btc == 0:
+                 btc_prices = []
+             if args.btc and args.usd == 0:
+                 usd_prices = []
+
+             if i == 'btc' and args.usd:
+                 print (i,":\t","{:.4f}".format(float(data[i])),"USD")
+             elif i == 'btc':
+                 pass
              else:
-                 print (i,":\t",
-                   "{0:.3f}".format(float(data[i])*float(data['btc'])),'USD',
-                   "\t", data[i],"BTC")
+                 print (i,"\t", *usd_prices, *btc_prices)
+
 
         time.sleep(10)
 
