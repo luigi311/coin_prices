0aa319c3143eebf3272f203a092a2b0f931ab2cc
Luis Garcia <luigi311.lg@gmail.com>
2018-01-23 10:58:37 +0000

    Added bitcoin usd

--- coin_prices.py
+++ coin_prices.py
@@ -5,13 +5,15 @@ def check_prices(names):
     with urllib.request.urlopen("https://bittrex.com/api/v1.1/public/getmarketsummaries") as url:
         data = json.loads(url.read().decode())
         allPrices = data["result"]
-  
+ 
     exchange_names = ["BTC-"+x.upper() for x in names]
     coins_dict = dict.fromkeys(names)
-
+ 
     for i in allPrices:
         for c in exchange_names:
             indexOfName = exchange_names.index(c)
+            if (i["MarketName"] == 'USDT-BTC'):
+                coins_dict['btc'] = "{:.8f}".format(i["Last"])
             if (i["MarketName"] == c):
                 coins_dict[names[indexOfName]] = "{:.8f}".format(i["Last"])
 
@@ -19,12 +21,15 @@ def check_prices(names):
 
 try:
     while True:
-        names = ["strat","xvg","snt","gnt","pay","steem","ans"]
+        names = ["btc","strat","xvg","snt","gnt","pay","steem","neo"]
         data = check_prices(names)
         os.system('clear')
         print (strftime("%m-%d-%y %H:%M:%S", gmtime()))
         for i in names:
-             print (i+":\t",data[i],"BTC")
+             if i == 'btc':
+                 print (i+":\t",data[i],"USD")
+             else:
+                 print (i+":\t",data[i],"BTC")
 
         time.sleep(3)
 
