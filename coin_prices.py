44f48966db78ec4ca3da39a54245492f087aa815
Luis Garcia <luigi311.lg@gmail.com>
2018-01-24 15:57:25 -0700

    Add aditional arguments allowing source changing. Add coinmarketcap support. Changed float casting location to reduce the amount of times casting is done. Add print function that will handle the print output format.

--- coin_prices.py
+++ coin_prices.py
@@ -3,11 +3,28 @@ import urllib.request, json, time, os
 from time import gmtime, strftime
 
 parser = argparse.ArgumentParser()
+parser.add_argument("source", type=str, help="Source where to grab prices")
+parser.add_argument("--base", help="Set the base pair value instead of the default bitcoin")
 parser.add_argument("--btc", help="Display Bitcoin prices", action="store_true")
 parser.add_argument("--usd", help="Display USD prices", action="store_true")
 args = parser.parse_args()
 
+coinmarketcap_api = "https://api.coinmarketcap.com/v1/ticker/"
 bittrex_api = "https://bittrex.com/api/v1.1/public/getmarketsummaries"
+
+def coinmarketcap(coin_names):
+    coins_dict = dict.fromkeys(coin_names)
+
+    with urllib.request.urlopen(coinmarketcap_api) as url:
+        allPrices = json.loads(url.read().decode())
+
+    for i in allPrices:
+        for c in coin_names:
+            indexOfName = coin_names.index(c)
+            if (i["symbol"] == c.upper()):
+                coins_dict[coin_names[indexOfName]] = float(i["price_usd"])
+
+    return coins_dict
 
 def bittrex(coin_names):
     with urllib.request.urlopen(bittrex_api) as url:
@@ -21,34 +38,43 @@ def bittrex(coin_names):
         for c in exchange_names:
             indexOfName = exchange_names.index(c)
             if (i["MarketName"] == 'USDT-BTC'):
-                coins_dict['btc'] = "{:.8f}".format(i["Last"])
+                coins_dict['btc'] = float("{:.8f}".format(i["Last"]))
             if (i["MarketName"] == c):
-                coins_dict[coin_names[indexOfName]] = "{:.8f}".format(i["Last"])
+                coins_dict[coin_names[indexOfName]] = float("{:.8f}".format(i["Last"]))
 
     return coins_dict
+
+def print_output(coin_names,data):
+    for i in coin_names:
+        if args.source == "coinmarketcap":
+            usd_prices = [data[i],'USD']
+            btc_prices = ["{:.8f}".format(data[i]/data["btc"]),"BTC"]
+
+        else:
+            if i == "btc":
+                usd_prices = [data[i],"USD"]
+            else:
+                usd_prices = ["{:.4f}".format(data[i]*data['btc']),'USD']
+            btc_prices = [data[i],"BTC"]
+
+        if args.usd and args.btc == 0:
+            btc_prices = []
+        if args.btc and args.usd == 0:
+            usd_prices = []
+
+        print (i,"\t", *usd_prices, "\t", *btc_prices)
 
 try:
     while True:
         coin_names = ["btc","strat","xvg","snt","gnt","pay","steem","neo"]
-        data = bittrex(coin_names)
+        function_map = {"coinmarketcap":coinmarketcap,"bittrex":bittrex}
+
+        coin_source = function_map[args.source]
+        data = coin_source(coin_names)
+
         os.system('clear')
-
         print (strftime("%m-%d-%y %H:%M:%S", gmtime()))
-        for i in coin_names:
-             usd_prices = ["{:.4f}".format(float(data[i])*float(data['btc'])),'USD']
-             btc_prices = [data[i],"BTC"]
-
-             if args.usd and args.btc == 0:
-                 btc_prices = []
-             if args.btc and args.usd == 0:
-                 usd_prices = []
-
-             if i == 'btc' and args.usd:
-                 print (i,"\t","{:.4f}".format(float(data[i])),"USD")
-             elif i == 'btc':
-                 pass
-             else:
-                 print (i,"\t", *usd_prices, *btc_prices)
+        print_output(coin_names,data)
 
         time.sleep(60)
 
